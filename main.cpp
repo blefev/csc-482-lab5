33541335f301739570df1b0f1f116fa7ccd26534
blefev <blefevre000@gmail.com>
2019-10-26 19:05:24 -0600

    namesToFuncs working

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@
 #include <string>
 #include <iostream>
 #include <map>
+#include <typeinfo>
 
 using namespace std;
 
-double timeFunction(function< uint64_t(uint64_t) > func, uint64_t param);
+double timeFunction(string funcName, uint64_t param);
 bool testAllFibFuncs();
-bool testFibFunc(function< uint64_t(uint64_t) > func, string funcName, uint8_t n);
-void observeAllFuncs();
+bool testFibFunc(string funcName, uint8_t n);
+void measureAllFuncs();
 void populateNamesToFuncs();
 
 map<string, function<uint64_t(uint64_t)>> namesToFuncs;
@@ -20,7 +21,7 @@ int main()
 {
 	populateNamesToFuncs();
 	testAllFibFuncs();
-	observeAllFuncs();
+	measureAllFuncs();
 
 	return 0;
 }
@@ -32,24 +33,26 @@ void populateNamesToFuncs() {
 	namesToFuncs.emplace("FibMatrix", FibMatrix);
 }
 
-void observeAllFuncs() {
+void measureAllFuncs() {
 	
 }
 
 bool testAllFibFuncs() {
-	return	testFibFunc(FibLoop, "FibLoop", 45) &&
-			testFibFunc(FibRecur, "FibRecur", 45) &&
-			testFibFunc(FibRecurDP, "FibRecurDP", 94) &&
-			testFibFunc(FibMatrix, "FibMatrix", 94);	
+	return	testFibFunc("FibLoop", 45) &&
+			testFibFunc("FibRecur", 45) &&
+			testFibFunc("FibRecurDP", 94) &&
+			testFibFunc("FibMatrix", 94);	
 }
 
-bool testFibFunc(function< uint64_t(uint64_t) > func, string funcName, uint8_t n) {
+bool testFibFunc(string funcName, uint8_t n) {
 	// First 95 fibonaccis (including zero)
 	// fibonacci(94) is the max uint64_t can handle
 	cout << "Testing " << funcName << "\n";
 
 	for (uint64_t i = 0; i < n; i++) {
+		function<uint64_t(uint64_t)> func = namesToFuncs.at(funcName);
 		uint64_t result = func(i);
+
 		if (result != FIBS[i]) {
 			cout << funcName << "(" << i << "): failed\n";
 			cout << "Expected: " << FIBS[i] << ".Got : " << result << "\n";
@@ -60,11 +63,12 @@ bool testFibFunc(function< uint64_t(uint64_t) > func, string funcName, uint8_t n
 	return true;
 }
 
-double timeFunction(function< uint64_t(uint64_t) > func, uint64_t param) {
+double timeFunction(string funcName, uint64_t param) {
 	using namespace chrono;
 
 	high_resolution_clock::time_point start = high_resolution_clock::now();
-
+	
+	function<uint64_t(uint64_t)> func = namesToFuncs.at(funcName);
 	func(param);
 		
 	high_resolution_clock::time_point end = high_resolution_clock::now();
